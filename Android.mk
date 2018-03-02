3cbd02e87b47e5318399ccadc37f051f95843f18
nasreirma <nasreirma@gmail.com>
2018-03-02 14:21:34 +0000

    Bringup los-15.1

--- Android.mk
+++ Android.mk
@@ -1,29 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter X556, $(TARGET_DEVICE)),)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libaudiocustparam
-LOCAL_SRC_FILES_64 := vendor/lib64/libaudiocustparam.so
-LOCAL_MULTILIB := 64
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
 LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libmtkcam_fwkutils
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 64
-LOCAL_SRC_FILES_64 = vendor/lib64/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
  
 include $(CLEAR_VARS)
@@ -32,14 +15,6 @@ LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX = .so
 LOCAL_MULTILIB = 32
 LOCAL_SRC_FILES_32 = vendor/lib/libmtkcam_fwkutils.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 64
-LOCAL_SRC_FILES_64 = vendor/lib64/libdpframework.so
 include $(BUILD_PREBUILT)
  
 include $(CLEAR_VARS)
@@ -51,23 +26,57 @@ LOCAL_SRC_FILES_32 = vendor/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE = libstagefright_color_conversion
-LOCAL_MODULE_CLASS = STATIC_LIBRARIES
-LOCAL_MODULE_SUFFIX = .a
-LOCAL_UNINSTALLABLE_MODULE = true
-LOCAL_MULTILIB = 64
-LOCAL_SHARED_LIBRARIES_64 = libdpframework
-LOCAL_SRC_FILES_64 = libstagefright_color_conversion/libstagefright_color_conversion_64.a
+LOCAL_MODULE = libion_mtk
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = vendor/lib/libion_mtk.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libged
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = vendor/lib/libged.so
+include $(BUILD_PREBUILT)
+
+LOCAL_MODULE := libaudiocustparam
+LOCAL_SRC_FILES_64 := vendor/lib64/libaudiocustparam.so
+LOCAL_MULTILIB := 64
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
  
 include $(CLEAR_VARS)
-LOCAL_MODULE = libstagefright_color_conversion
-LOCAL_MODULE_CLASS = STATIC_LIBRARIES
-LOCAL_MODULE_SUFFIX = .a
-LOCAL_UNINSTALLABLE_MODULE = true
-LOCAL_MULTILIB = 32
-LOCAL_SHARED_LIBRARIES_32 = libdpframework
-LOCAL_SRC_FILES_32 = libstagefright_color_conversion/libstagefright_color_conversion_32.a
+LOCAL_MODULE = libmtkcam_fwkutils
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 = vendor/lib64/libmtkcam_fwkutils.so
+include $(BUILD_PREBUILT)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 = vendor/lib64/libdpframework.so
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE = libion_mtk
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 = vendor/lib64/libion_mtk.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libged
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 = vendor/lib64/libged.so
+include $(BUILD_PREBUILT)
 endif
