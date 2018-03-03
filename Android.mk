87dd5c3e25bdd77fc3036363da8797affd8db8c9
nasreirma <nasreirma@gmail.com>
2018-03-03 11:48:06 +0000

    fix

--- Android.mk
+++ Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter X556, $(TARGET_DEVICE)),)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
 LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
@@ -34,42 +35,19 @@ LOCAL_SRC_FILES_32 = vendor/lib/libion_mtk.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE = libged
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = vendor/lib/libged.so
-include $(BUILD_PREBUILT)
-
-LOCAL_MODULE := libaudiocustparam
-LOCAL_SRC_FILES_64 := vendor/lib64/libaudiocustparam.so
-LOCAL_MULTILIB := 64
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
- 
-include $(CLEAR_VARS)
-LOCAL_MODULE = libmtkcam_fwkutils
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 64
-LOCAL_SRC_FILES_64 = vendor/lib64/libmtkcam_fwkutils.so
-include $(BUILD_PREBUILT)
- 
-include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 64
-LOCAL_SRC_FILES_64 = vendor/lib64/libdpframework.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE = libion_mtk
 LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX = .so
 LOCAL_MULTILIB = 64
 LOCAL_SRC_FILES_64 = vendor/lib64/libion_mtk.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libged
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = vendor/lib/libged.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
