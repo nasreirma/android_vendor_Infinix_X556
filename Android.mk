b62e169cb43769e358ab18040ccebee05016d41e
Nasrellah Irhmar <nasreirma@gmail.com>
2018-03-31 21:29:38 +0200

    Initial commit

--- Android.mk
+++ Android.mk
@@ -1,25 +1,48 @@
 LOCAL_PATH := $(call my-dir)
 
-$(info copying SHARED proprietary blobs)
-
+ifneq ($(filter X556, $(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
-LOCAL_MODULE_OWNER := MediaTek
-LOCAL_SRC_FILES_64 := proprietary/lib64/libaudiocustparam.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libaudiocustparam.so
+LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
+LOCAL_SRC_FILES_64 := vendor/lib64/libaudiocustparam.so
 LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE = libmtkcam_fwkutils
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = both
+LOCAL_SRC_FILES_32 = vendor/lib/libmtkcam_fwkutils.sov
+LOCAL_SRC_FILES_64 = vendor/lib64/libmtkcam_fwkutils.so
+include $(BUILD_PREBUILT)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = both
+LOCAL_SRC_FILES_32 = vendor/lib/libdpframework.so
+LOCAL_SRC_FILES_64 = vendor/lib64/libdpframework.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libdpframework
-LOCAL_MODULE_OWNER := MediaTek
-LOCAL_SRC_FILES_64 := proprietary/lib64/libdpframework.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE = libion_mtk
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = both
+LOCAL_SRC_FILES_32 = vendor/lib/libion_mtk.so
+LOCAL_SRC_FILES_64 = vendor/lib64/libion_mtk.so
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libged
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = both
+LOCAL_SRC_FILES_32 = vendor/lib/libged.so
+LOCAL_SRC_FILES_64 = vendor/lib64/libged.so
+include $(BUILD_PREBUILT)
+endif
