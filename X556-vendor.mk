78d8536f00ac296f3e31a41a3c9c5b40a62085e2
Nasrellah Irhmar <nasreirma@gmail.com>
2018-04-05 05:25:56 +0100

    add lib 64 section

--- X556-vendor.mk
+++ X556-vendor.mk
@@ -8,4 +8,6 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/vendor
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/vendor/lib,system/vendor/lib)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/vendor/lib64,system/vendor/lib64)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/vendor/thh,system/vendor/thh)
