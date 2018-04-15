3d575afe81297d76ebfe7aec44bc82f5494b3161
nasreirma <nasreirma@gmail.com>
2018-04-15 13:27:47 +0000

    update

--- Android.mk
+++ Android.mk
@@ -4,7 +4,8 @@ ifneq ($(filter X556, $(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
 LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
-LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_64 := vendor/lib64/libaudiocustparam.so
+LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
@@ -13,16 +14,28 @@ include $(CLEAR_VARS)
 LOCAL_MODULE = libmtkcam_fwkutils
 LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
+LOCAL_MULTILIB = both
 LOCAL_SRC_FILES_32 = vendor/lib/libmtkcam_fwkutils.so
+LOCAL_SRC_FILES_64 = vendor/lib64/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
  
 include $(CLEAR_VARS)
 LOCAL_MODULE = libdpframework
 LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
+LOCAL_MULTILIB = both
 LOCAL_SRC_FILES_32 = vendor/lib/libdpframework.so
+LOCAL_SRC_FILES_64 = vendor/lib64/libdpframework.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libstagefright_color_conversion
+LOCAL_MODULE_CLASS = STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX = .a
+LOCAL_UNINSTALLABLE_MODULE = true
+LOCAL_MULTILIB = 64
+LOCAL_SHARED_LIBRARIES_64 = libdpframework
+LOCAL_SRC_FILES_64 = libstagefright_color_conversion/libstagefright_color_conversion_64.a
 include $(BUILD_PREBUILT)
  
 include $(CLEAR_VARS)
--- X556-vendor.mk
+++ X556-vendor.mk
@@ -19,3 +19,14 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/vendor
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/vendor/lib64,system/vendor/lib64)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/vendor/thh,system/vendor/thh)
+
+# Add for FP
+ifeq ($(strip $(FINGERPRINT_GOODIX_SUPPORT_GF368M)), yes)
+PRODUCT_COPY_FILES += \
+	vendor/Infinix/X556/fingerprint/goodix/GX368/x64/fingerprint.default.so:system/lib64/hw/fingerprint.default.so \
+	vendor/Infinix/X556/fingerprint/goodix/GX368/x64/gxfingerprint.default.so:system/lib64/hw/gxfingerprint.default.so \
+	vendor/Infinix/X556/fingerprint/goodix/GX368/x64/libfp_client.so:system/lib64/libfp_client.so \
+	vendor/Infinix/X556/fingerprint/goodix/GX368/x64/libfpservice.so:system/lib64/libfpservice.so \
+	vendor/Infinix/X556/fingerprint/goodix/GX368/x64/libalgoandroid.so:system/lib64/libalgoandroid.so \
+	vendor/Infinix/X556/fingerprint/goodix/GX368/x64/gx_fpd:system/bin/gx_fpd
+endif
