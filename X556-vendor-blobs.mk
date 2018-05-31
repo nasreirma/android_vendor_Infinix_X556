e8ff3d1dc6188a0569319a8d19913a6e628dd613
nasreirma <nasreirma@gmail.com>
2018-05-31 23:34:10 +0000

    add substratum

--- X556-vendor-blobs.mk
+++ X556-vendor-blobs.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/proprietary/app,system/app)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/proprietary/bin,system/bin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Infinix/X556/proprietary/data,system/data)
